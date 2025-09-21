39c90029e45aaafbee0c79f13e9219c908200d39
sgandhi15 <gandhi.sarthak15@gmail.com>
2025-09-21 16:59:46 -0230

    feat: nest-swagger module added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('The ecommerce API developed using NestJS')
+    .setVersion('1.0')
+    .addTag('ecommerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
